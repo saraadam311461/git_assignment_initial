6dca8e4010d5fc4b22945829928835b1f05fcab4
Youssef Hammad <32943220+Youssef-Hammad@users.noreply.github.com>
2019-11-24 20:51:47 +0200

    Update MultiplicationSeries.java

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class MultiplicationSeries {
+public class MultiplicationSeries extends ThreadSubscriber {
+    @Override
+    public void notifySubscriber() {
+        double input = topic.getInput();
+        int fact = 1;
+        for(int i = 1; i<= input; i++)
+            fact*=i;
+        System.out.println("Factorial of "+input+": "+fact);
+    }
 }
