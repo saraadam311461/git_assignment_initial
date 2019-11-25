4e9ee116798a5fd7744eb26835f80f946286c532
Youssef Hammad <32943220+Youssef-Hammad@users.noreply.github.com>
2019-11-25 02:06:16 +0200

    Update MultiplicationSeries.java

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -4,19 +4,26 @@ public class MultiplicationSeries extends ThreadSubscriber implements ObserverTe
     @Override
     public void notifySubscriber() {
         input = topic.getInput();
-        int fact = 1;
-        for(int i = 1; i<= input; i++)
-            fact*=i;
-        System.out.println("Factorial of "+input+": "+fact);
+        System.out.println("Factorial of "+input+": "+execute());
     }
 
     @Override
     public String execute() {
-        return null;
+        int fact = 1;
+        for(int i = 1; i<= input; i++)
+            fact*=i;
+        String res = String.valueOf(fact);
+        return res;
     }
 
     @Override
     public Boolean test() {
-        return null;
+        input = 1;
+        if(!execute().equals("1"))
+            return false;
+        input=5;
+        if(!execute().equals("120"))
+            return false;
+        return true;
     }
 }
