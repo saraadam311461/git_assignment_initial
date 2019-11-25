0102b665092633754904bae4d8c10b85837ad246
AMarwan <amarwan43@gmail.com>
2019-11-25 20:31:04 +0200

    initial commit

---
+++ MainTest.java
@@ -0,0 +1,20 @@
+public class MainTest {
+    private static ObserverTest subscribers [] = {
+            new SphereArea() ,
+            new CircleArea(),
+            new CircleCircumference(),
+            new SummationSeries(),
+            new MultiplicationSeries(),
+            new CircleVolum(),
+	        new LucasSeries(),
+	        new Fibo(),
+    };
+    public static void main(String[] args) {
+        for (ObserverTest sub : subscribers){
+            Boolean res = sub.test();
+            if(res!=null && res==false){
+                System.exit(1);
+            }
+        }
+    }
+}
